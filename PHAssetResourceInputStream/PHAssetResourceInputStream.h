9b43914a26c3ac6762a3b1af01521b5284ed090a
Aleksandr Dvornikov <advrnkov@yandex-team.ru>
2016-08-23 14:49:56 +0300

    Initial Commit

---
+++ PHAssetResourceInputStream/PHAssetResourceInputStream.h
@@ -0,0 +1,19 @@
+//
+//  PHAssetResourceInputStream.h
+//  PHAssetResourceInputStream
+//
+//  Created by Aleksandr Dvornikov on 23/08/16.
+//  Copyright © 2016 Aleksandr Dvornikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHAssetResourceInputStream.
+FOUNDATION_EXPORT double PHAssetResourceInputStreamVersionNumber;
+
+//! Project version string for PHAssetResourceInputStream.
+FOUNDATION_EXPORT const unsigned char PHAssetResourceInputStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHAssetResourceInputStream/PublicHeader.h>
+
+
